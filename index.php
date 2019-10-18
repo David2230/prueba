e5205db00cb10c7712f34c45dc56a61410a211e9
soa-a208-e-011 <soa-a208-e-011@uniminuto.edu>
2019-10-17 21:30:06 -0500

    cambio

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
 	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
+	
 	$uri .= $_SERVER['HTTP_HOST'];
 	header('Location: '.$uri.'/dashboard/');
 	exit;
